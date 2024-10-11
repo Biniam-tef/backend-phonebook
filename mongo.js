378f1a88fd359c67d6affbaf280ee3eeb18ffe33
Biniam Tefrie <79765727+beni1218@users.noreply.github.com>
2024-10-11 09:12:42 +0300

    Code editing

--- mongo.js
+++ mongo.js
@@ -1,52 +1,52 @@
 const mongoose = require('mongoose');
 
 if (process.argv.length < 3) {
-  console.log('give password as argument');
-  process.exit(1);
+  console.log('give password as argument')
+  process.exit(1)
 }
 
-const password = process.argv[2];
-const name = process.argv[3];
-const number = process.argv[4];
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
 
-const url = `mongodb+srv://phonebook:${password}@cluster0.dld8y.mongodb.net/Phonebook?retryWrites=true&w=majority&appName=Cluster0`;
+const url = `mongodb+srv://phonebook:${password}@cluster0.dld8y.mongodb.net/Phonebook?retryWrites=true&w=majority&appName=Cluster0`
 
-mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', false)
 
 // Define the schema
 const phoneSchema = new mongoose.Schema({
   name: String,
   number: String,
-});
+})
 
 // Define the model
-const Phone = mongoose.model('Phone', phoneSchema);
+const Phone = mongoose.model('Phone', phoneSchema)
 
 // Connect to MongoDB
 mongoose.connect(url)
   .then(() => {
-    console.log('Connected to MongoDB');
+    console.log('Connected to MongoDB')
 
     // If name and number are provided, add new entry
     if (name && number) {
       const newPhoneEntry = new Phone({ name, number });
       return newPhoneEntry.save().then(() => {
-        console.log(`Added ${name} number ${number} to phonebook`);
-        return Phone.find({});
+        console.log(`Added ${name} number ${number} to phonebook`)
+        return Phone.find({})
       });
     } else {
       // If no new name and number are provided, just print all the existing data
-      return Phone.find({});
+      return Phone.find({})
     }
   })
   .then(result => {
-    console.log('Phonebook entries:');
+    console.log('Phonebook entries:')
     result.forEach(entry => {
-      console.log(`${entry.name} ${entry.number}`);
+      console.log(`${entry.name} ${entry.number}`)
     });
-    mongoose.connection.close();
+    mongoose.connection.close()
   })
   .catch(err => {
-    console.error('Error:', err.message);
-    mongoose.connection.close();
+    console.error('Error:', err.message)
+    mongoose.connection.close()
   });
